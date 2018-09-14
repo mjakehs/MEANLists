7fa6bbc00b32f43ea5901de738b29c648f875232
Jake Marzitelli <mjakehs@gmail.com>
2018-09-14 16:36:18 -0500

    add database connection

---
+++ server/database-connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const databaseURL = 'mongodb://localhost:27017/tasklist';
+mongoose.connect(databaseURL, {useNewUrlParser: true});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database at: ', databaseURL);
+})
+mongoose.connection.on('error', (error) => {
+    console.log('Error connection to database: ', error);
+})
+
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+//const tasks = require('./modules/routes/task.router');
 const app = express();
 const PORT = 5000;
+require('./database-connection');
+
 
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
+//app.use('/tasks', tasks);
 
 app.listen(PORT, () => {
     console.log('Server is running on localhost: ', PORT);
